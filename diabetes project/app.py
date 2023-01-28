84c25212181ebf553c250e40342fb258c205cf0f
Amarnadh K <amar6949nadh@gmail.com>
2023-01-28 10:57:23 +0530

    initial commit

---
+++ diabetes project/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+import pickle
+
+app = Flask(__name__)
+
+model = pickle.load(open('savedmodel.sav', 'rb'))
+
+@app.route('/')
+
+def home():
+    result = ' '
+    return render_template('home.html', **locals())
+
+
+
+
+@app.route('/predict', methods=[ 'POST', 'GET'])
+def predict():
+    Pregnancies = float(request.form['Pregnancies'])
+    Glucose = float(request.form['Glucose'])
+    BloodPressure = float(request.form['BloodPressure'])
+    SkinThickness = float(request.form['SkinThickness'])
+    Insulin = float(request.form['Insulin'])
+    BMI = float(request.form['BMI'])
+    DiabetesPedigreeFunction = float(request.form['DiabetesPedigreeFunction'])
+    Age = float(request.form['Age'])
+    result = model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
+    return render_template('home.html', **locals())
+
+if __name__ == '__main__':
+    app.run(debug=True)
